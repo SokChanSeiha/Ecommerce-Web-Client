7d46dcb5e4bec858d41e1ad5167c3b405dc7c713
SokChanSeiha <sokchanseiha@gmail.com>
2026-07-26 22:21:43 +0700

    create component sign-in and sign-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SignIn } from './sign-in/sign-in';
+import { SignUp } from './sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sign-in',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-in',
+    component: SignIn,
+  },
+  {
+    path: 'sign-up',
+    component: SignUp,
+  },
+];
