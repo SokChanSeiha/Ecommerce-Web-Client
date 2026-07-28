89bc0a3f61cc19591aec87b91f04a6954a3b8d31
SokChanSeiha <sokchanseiha@gmail.com>
2026-07-28 12:45:17 +0700

    test using component UI

--- src/app/signup-page/signup-page.ts
+++ src/app/signup-page/signup-page.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-signup-page',
-  imports: [],
+  imports: [MatProgressBarModule, MatCardModule, MatChipsModule],
   templateUrl: './signup-page.html',
   styleUrl: './signup-page.css',
 })
-export class SignupPage {}
+export class SignupPage {
+    longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+}
+
+
